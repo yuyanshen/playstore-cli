5db6c1eb1587e53ec8dea66d7acd13a1d10e6591
於琰珅 <yuyanshen@yuyanshendeMacBook-Pro.local>
2019-12-21 00:53:30 +0800

    update

--- actions/updatePricingPlace.js
+++ actions/updatePricingPlace.js
@@ -11,29 +11,29 @@ module.exports = async (self, app, metadata) => {
   let tag = self.tag + chalk.cyan(' :updatePrice')
 
   console.log(tag, 'Opening form')
-  await sleep(1000)
+  await sleep(3000)
   await page.goto(`${self.PlayURL}#PricingPlace:p=${app.package_name}`)
-
+  await sleep(3000)
   console.log(tag, 'Waiting to load')
-  await sleep(2000)
+  await sleep(3000)
   await page.waitForSelector('input[name="countryheader"]')
-  await sleep(2000)
+  await sleep(3000)
   // Set to all countries
   console.log(tag, 'Filling form')
   await Pupt.checkRadio(page, 'input[name="countryheader"]', 1)
-  
+  await sleep(3000)
   // Set to NO on infant public
   await Pupt.checkRadio(page, 'input[name="pcdRadioButton"]', 1)
-  
+  await sleep(3000)
   // Set to NO for ads
   await Pupt.checkRadio(page, 'input[name="ads_supported"]', 1)
-  
+  await sleep(3000)
   // Agree to terms (1)
   Pupt.check(page, 'fieldset > label:nth-child(3) > div > div input[type="checkbox"]')
-
+  await sleep(3000)
   // Agree to terms (2)
   Pupt.check(page, 'fieldset > label:nth-child(4) > div > div input[type="checkbox"]')
-
+  await sleep(3000)
   console.log(tag, 'Saving...')
   await self.saveForm()
 }
